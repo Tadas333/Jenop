2fe0aa2c8084fe2460c828a7f40c3227c1131d59
Tadas <42923022+Tadas333@users.noreply.github.com>
2021-09-15 15:04:46 -0400

    Add files via upload

--- timestamp_renamer.py
+++ timestamp_renamer.py
@@ -2,27 +2,35 @@ import cv2
 import numpy as np 
 import glob, os 
 from datetime import datetime
+import time, sys
 
 #path1 = 'C:/Users/tadas.orentas/Desktop/PROJECTS/Vector_Streaming_solution'
 #path1 = 'C:/Users/tadas.orentas/Desktop/PROJECTS/Vector_Streaming_solution/vid1.mp4'
 
-path1 = r'C:/Users/tadas.orentas/Desktop/PROJECTS/Vector_Streaming_solution'
-path2 = r'C:/Users/tadas.orentas/Desktop/PROJECTS/Vector_Streaming_solution/'
+path1 = r'C:/Users/tadas.orentas/Desktop/PROJECTS/Vector_Streaming_solution/outputs'
+path2 = r'C:/Users/tadas.orentas/Desktop/PROJECTS/Vector_Streaming_solution/changed/'
+path3 = r'C:/Users/tadas.orentas/Desktop/PROJECTS/Vector_Streaming_solution/changed'
 
 
+while(1):
 
-#for vinput in glob.glob(os.path.join(path1, '*.mp4')):
-for vinput in os.listdir(path1): 
-    if vinput.endswith('.mp4'):
+    for vinput in os.listdir(path1): 
+        if vinput.endswith('.mp4'):
 
-        now = datetime.now()
-        dt_string = now.strftime("%d_%m_%Y__%H_%M_%S")
-        
+            now = datetime.now()
+            dt_string = now.strftime("%d_%m_%Y__%H_%M_%S")
+            
 
-        #print(dt_string)
-        
-        new_name =  dt_string + ".mp4"
-        os.rename(vinput, path2 + new_name)
+            #print(dt_string)
+            
+            new_name =  dt_string + ".mp4"
+            os.rename(os.path.join(path1, vinput), os.path.join(path2, path2 + new_name))
+            now1 = time.time()
+            for f in os.listdir(path3):
+                f = os.path.join(path3, f)
+                if os.stat(f).st_mtime < now1 - 50:
+                    os.remove(os.path.join(path3, f))
+                    time.sleep(10)
     
 
 
